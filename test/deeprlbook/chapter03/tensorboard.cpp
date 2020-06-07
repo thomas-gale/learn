916b7296b6e4c47004b71dc3ea8254f85dd7e8b1
Thomas Gale <gale.thomasjames@gmail.com>
2020-06-07 12:52:46 +0100

    Tensorboard example from Deep RL Book.
    Re-writing as c++ from python. Not as elegent!
    Reinveting the wheel, but worse :)
    Good learning exercise.

--- test/deeprlbook/chapter03/tensorboard.cpp
+++ test/deeprlbook/chapter03/tensorboard.cpp
@@ -1,9 +1,28 @@
 #include "gtest/gtest.h"
-#include "learn/example.h"
 
-namespace TestLearn {
-    TEST(GymClient, RunExample) {
-        // Act
-        learn::example();
+#include <cmath>
+#include "learn/logging/summarywriter.h"
+
+using namespace learn;
+
+namespace testlearn {
+    TEST(DRLChapter03, CanUseTensorboard) {
+        SummaryWriter writer;
+
+        std::vector<std::tuple<std::string, std::function<float(float)>>> funcs{
+            { "Sin", [](float x){ return std::sin(x); } },
+            { "Cos", [](float x){ return std::cos(x); } },
+            { "Tan", [](float x){ return std::tan(x); } }
+        };
+
+        float pi = std::atan(1)*4.0;
+        for (int angle = -360; angle <= 360; ++angle) {
+            float angleRad = float(angle) * (pi / 180.0);
+            for (const auto& func : funcs) {
+                float val = std::get<1>(func)(angleRad);
+                bool success = writer.addScalar(std::get<0>(func), val, angle);
+                ASSERT_TRUE(success);
+            }
+        }
     }
 }
